8a93d6896b779ceb064caa4f45fa0cccd783919c
Subin Cho <sbjo7713@gmail.com>
2022-06-28 01:29:49 +0900

    antd prolayout initial settings

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,28 @@
+import { DefaultFooter } from '@ant-design/pro-components';
+import { useIntl } from 'umi';
+
+const Footer: React.FC = () => {
+  const intl = useIntl();
+  const defaultMessage = intl.formatMessage({
+    id: 'app.copyright.produced',
+    defaultMessage: '蚂蚁集团体验技术部出品',
+  });
+
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <DefaultFooter
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: 'Ant Design Pro',
+          title: 'Ant Digimon',
+          href: 'https://pro.ant.design',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+
+export default Footer;
