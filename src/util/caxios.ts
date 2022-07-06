a3ac1638a46cac7f3def068d05da3de76f976e74
jinsb1995 <jinsb1995@gmail.com>
2022-07-06 23:36:32 +0900

    add axios interceptor

--- src/util/caxios.ts
+++ src/util/caxios.ts
@@ -1,5 +1,6 @@
 import type { Axios } from 'axios';
 import axios from 'axios';
+import { history } from 'umi';
 import apiServer from '../../config/apiServer';
 
 const { NODE_ENV } = process.env;
@@ -10,5 +11,13 @@ caxios.defaults.baseURL = `${apiServer[NODE_ENV || ''].prefix}`;
 caxios.defaults.timeout = 100000;
 caxios.defaults.withCredentials = true;
 caxios.defaults.headers.common['Content-Type'] = 'application/json';
+caxios.interceptors.request.use((error) => {
+  console.log('request');
+  history.push('/nologin/maps');
+});
 
+caxios.interceptors.response.use((error) => {
+  console.log('response');
+  console.log(error);
+});
 export default caxios;
